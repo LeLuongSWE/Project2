f76a30f0b8244d1aaa7c2dd23c1ffd7979f1406d
LeLuongSWE <luongswe@gmail.com>
2025-02-14 14:40:06 +0700

    Initial project 2
    Initial project 2. Tech stack:
    - frontend : react, vite
    - backend : spring boot
    - database : mysql

---
+++ backend/src/main/java/net/project2/backend/DemoApplication.java
@@ -0,0 +1,13 @@
+package net.project2.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
