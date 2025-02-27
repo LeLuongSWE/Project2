2fae8845205ddaede3222278617e4ee6c5474596
LeLuongSWE <luongswe@gmail.com>
2025-02-28 01:35:00 +0700

    a

---
+++ backend/src/main/java/net/project2/demo/RestaurantsWebAppApplication.java
@@ -0,0 +1,13 @@
+package net.project2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantsWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantsWebAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/net/project2/demo/RestaurantsWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.project2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantsWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
