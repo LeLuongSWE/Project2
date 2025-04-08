3d7e475d1e6eee16a2f6cc0a0b509965f3cbc36e
LeLuongSWE <luongswe@gmail.com>
2025-04-08 20:34:43 +0700

    Thêm diagram

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -1,10 +1,23 @@
 import Logo from "../../assets/logoco.png";
+import "./style.css";
 
 function Header() {
   return (
-    <header>
-      <img src={Logo} alt={"Logo"}></img>
-    </header>
+    <div>
+      <header id="header">
+        <img id="logo" src={Logo} alt={"Logo"}></img>
+        <button id="booking-button">Book a table</button>
+      </header>
+      <nav id="nav">
+        <a id="home" href="/">HOME</a>
+        <a id="about" href="#about">ABOUT</a>
+        <a id="menu" href="#menu">MENU</a>
+        <a id="event" href="#event">EVENT</a>
+        <a id="location" href="#location">LOCATION</a>
+        <a id="gallery" href="#gallery">GALLERY</a>
+        <a id="contact" href="#contact">CONTACT US</a>
+      </nav>
+    </div>
   );
 }
 
